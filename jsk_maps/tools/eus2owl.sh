3b664fb211e7e46198e328557729cd953230f1da
manabu <manabu@jsk.t.u-tokyo.ac.jp>
2011-07-05 15:50:24 +0000

    add room83b1, change to switchable the output of semantic_map converter

--- jsk_maps/tools/eus2owl.sh
+++ jsk_maps/tools/eus2owl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 MAP=eng2
+#MAP=scene1
 
 if [ -n "$1" ]; then
     MAP=$1
 fi
 
-rosrun roseus roseus `rospack find jsk_maps`/tools/convert-iasmap.l "(progn (yaml) (exit))"
+rosrun roseus roseus `rospack find jsk_maps`/tools/convert-iasmap.l "(progn (yaml :$MAP) (exit))"
 
 rosrun jsk_maps yaml2owl.py $MAP.yaml > $MAP.owl.in
 
