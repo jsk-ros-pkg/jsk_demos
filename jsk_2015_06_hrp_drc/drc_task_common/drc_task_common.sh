cc6358449c3d2053a2ac0b230cff0cfdcfb9750c
ohara <ohara@ohara-dynabook-T654-68KB.(none)>
2014-12-09 18:50:31 +0900

    added manipulation_data_processor in launch file

--- jsk_2015_06_hrp_drc/drc_task_common/drc_task_common.sh
+++ jsk_2015_06_hrp_drc/drc_task_common/drc_task_common.sh
@@ -32,5 +32,7 @@ tmux-newwindow robot_model "roslaunch drc_task_common robot_model.launch"
 tmux-newwindow transformable_model "roslaunch drc_task_common transformable_model.launch"
 tmux-newwindow teleop_device "roslaunch drc_task_common teleop_device.launch"
 tmux-newwindow teleop_interface "roslaunch drc_task_common teleop_interface.launch"
+tmux-newwindow manipulation_processor "roslaunch drc_task_common manipulation_data_processor.launch"
 tmux-newwindow rviz "rviz -d `rospack find drc_task_common`/config/drc_task_common.rviz"
+
 tmux send-keys -t drc:tmp "exit" C-m
