bcfd1d5bca2aaa77165bd42bd51ecf017f3570f6
Masaki Murooka <masaki.murooka@gmail.com>
2015-01-27 13:00:45 +0900

    add recognition parameter for valve with multisense

---
+++ jsk_2015_06_hrp_drc/drc_task_common/config/recog-param-for-multisense-valve/memo.sh
@@ -0,0 +1,3 @@
+rosrun dynamic_reconfigure dynparam load /euclidean_clustering euclidean_clustering.yaml
+rosrun dynamic_reconfigure dynparam load /multi_plane_estimate multi_plane_estimate.yaml
+rosrun dynamic_reconfigure dynparam load /multisense multisense.yaml
