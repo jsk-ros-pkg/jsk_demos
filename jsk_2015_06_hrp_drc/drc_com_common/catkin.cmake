be4c4d6308f7afd30e8ae9769c8dbe95b80e5f6e
Ryohei Ueda <garaemon@gmail.com>
2015-02-09 21:59:27 +0900

    [drc_com_common, drc_task_common] Add image_view2 based user interface. first step of system integration towards DRC final

--- jsk_2015_06_hrp_drc/drc_com_common/catkin.cmake
+++ jsk_2015_06_hrp_drc/drc_com_common/catkin.cmake
@@ -4,7 +4,7 @@ project(drc_com_common)
 find_package(catkin REQUIRED COMPONENTS cmake_modules message_generation std_msgs std_srvs sensor_msgs)
 
 add_message_files(FILES 
-  FC2OCSLarge.msg FC2OCSSmall.msg)
+  FC2OCSLarge.msg FC2OCSSmall.msg OCS2FCSmall.msg)
 generate_messages(DEPENDENCIES sensor_msgs)
 
 
