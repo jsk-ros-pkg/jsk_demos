894b33cb9a4d32712f74112d0fb9fabe29079bfc
Ryohei Ueda <garaemon@gmail.com>
2015-03-01 23:35:37 +0900

    [drc_task_common] Visualize status on rviz using OverlayText

---
+++ jsk_2015_06_hrp_drc/drc_task_common/scripts/rviz_status.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Time, ColorRGBA, UInt8
+from jsk_rviz_plugins.msg import OverlayText
+from drc_com_common.msg import FC2OCSLarge
+rospy.init_node("rviz_status")
+
+##########################################
+# useful variables
+##########################################
+OK_COLOR = ColorRGBA(r=91/255.0, g=255/255.0, b=255/255.0, a=1.0)
+WARN_COLOR = ColorRGBA(r=255/255.0, g=202/255.0, b=0/255.0, a=1.0)
+UNKNOWN_COLOR = ColorRGBA(r=251/255.0, g=221/255.0, b=221/255.0, a=1.0)
+TRANSPARENT_COLOR = ColorRGBA(r=0/255.0, g=0/255.0, b=0/255.0, a=0.0)
+
+##########################################
+# robot status
+# show text about if robot is moving
+##########################################
+pub_continuous_robot_status = rospy.Publisher("/ocs/rviz/visualization/continuous_robot_status", OverlayText)
+def robotStatusBasicInfoCallback(msg):
+    text = OverlayText()
+    text.bg_color = TRANSPARENT_COLOR
+    if msg.data == FC2OCSLarge.ROBOT_IDLE:
+        text.text = "NOW: Robot is idle"
+        text.fg_color = OK_COLOR
+    elif msg.data == FC2OCSLarge.ROBOT_MOVING:
+        text.text = "NOW: Robot is moving"
+        text.fg_color = WARN_COLOR
+    else:
+        text.text = "NOW: Robot is in unnknown status"
+        text.fg_color = UNKNOWN_COLOR
+    pub_continuous_robot_status.publish(text)
+sub_continuous_robot_status = rospy.Subscriber("/ocs/robot_status", UInt8, robotStatusBasicInfoCallback)
+
+pub_burst_robot_status = rospy.Publisher("/ocs/rviz/visualization/burst_robot_status", OverlayText)
+def robotStatusBasicInfoCallback(msg):
+    text = OverlayText()
+    text.bg_color = TRANSPARENT_COLOR
+    if msg.data == FC2OCSLarge.ROBOT_IDLE:
+        text.text = "BURST: Robot was idle"
+        text.fg_color = OK_COLOR
+    elif msg.data == FC2OCSLarge.ROBOT_MOVING:
+        text.text = "BURST: Robot was moving"
+        text.fg_color = WARN_COLOR
+    else:
+        text.text = "BURST: Robot was in unnknown status"
+        text.fg_color = UNKNOWN_COLOR
+    pub_burst_robot_status.publish(text)
+sub_burst_robot_status = rospy.Subscriber("/ocs/communication/robot_status", UInt8, robotStatusBasicInfoCallback)
+
+##########################################
+# communication status
+##########################################
+
+pub_continuous_data_updated_time = rospy.Publisher("/ocs/rviz/visualization/continuous_updated_time", OverlayText)
+def continuousDataUpdatedTimeCallnack(msg):
+    text = OverlayText()
+    now = rospy.Time.now()
+    diff = (now - msg.data).to_sec()
+    text.bg_color = TRANSPARENT_COLOR
+    text.text = "CONTINUOUS DATA: Updated %0.1f secs before" % (diff)
+    text.fg_color = OK_COLOR
+    pub_continuous_data_updated_time.publish(text)
+sub_continuous_data_updated_time = rospy.Subscriber("/ocs_from_fc_basic_low_speed/last_received_time", 
+                                                    Time, continuousDataUpdatedTimeCallnack)
+
+
+pub_lowspeed_data_updated_time = rospy.Publisher("/ocs/rviz/visualization/lowspeed_updated_time", OverlayText)
+def lowspeedDataUpdatedTimeCallnack(msg):
+    text = OverlayText()
+    now = rospy.Time.now()
+    diff = (now - msg.data).to_sec()
+    text.bg_color = TRANSPARENT_COLOR
+    text.text = "LOWSPEED DATA: Updated %0.1f secs before" % (diff)
+    text.fg_color = OK_COLOR
+    pub_lowspeed_data_updated_time.publish(text)
+sub_lowspeed_data_updated_time = rospy.Subscriber("/ocs_from_fc_low_speed/last_received_time", 
+                                                    Time, lowspeedDataUpdatedTimeCallnack)
+
+
+pub_burst_data_updated_time = rospy.Publisher("/ocs/rviz/visualization/burst_updated_time", OverlayText)
+def burstDataUpdatedTimeCallnack(msg):
+    text = OverlayText()
+    now = rospy.Time.now()
+    diff = (now - msg.data).to_sec()
+    text.bg_color = TRANSPARENT_COLOR
+    text.text = "BURST DATA: Updated %0.1f secs before" % (diff)
+    text.fg_color = OK_COLOR
+    pub_burst_data_updated_time.publish(text)
+sub_burst_data_updated_time = rospy.Subscriber("/highspeed_receiver/last_received_time", Time,
+                                               burstDataUpdatedTimeCallnack)
+
+rospy.spin()
