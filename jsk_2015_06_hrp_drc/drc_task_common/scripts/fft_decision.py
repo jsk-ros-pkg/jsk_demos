72da4824ca683410975dc58a26ce7b5e2a2bb759
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2015-04-02 19:04:54 +0900

    add codes for svm desicion

---
+++ jsk_2015_06_hrp_drc/drc_task_common/scripts/fft_decision.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+
+PKG='drc_task_common'
+
+import imp
+import scipy.fftpack
+import numpy as np
+import csv
+from sklearn import svm
+from drc_task_common.msg import Float32ArrayStamped
+
+
+## import message_filters
+def load_data(filename):
+    spamReader = csv.reader(open(filename, 'rb'), delimiter=' ', quotechar='|')
+    X = []
+    Y = []
+    for row in spamReader:
+        X.append(int(row[0]))
+        Y.append([float(x) for x in row[1:]])
+    return (X, Y)
+def disc_cb(msg):
+    print clf.predict(msg.data)
+if __name__ == "__main__":
+    rospy.init_node('fft_data', anonymous=True)
+    x, y = load_data("fft_data.csv")
+    print "tests"
+    print x
+    print y
+    clf = svm.SVC(kernel="rbf")
+    clf.fit(y, x)
+    rospy.Subscriber("input", Float32ArrayStamped, disc_cb)
+    rospy.spin()
