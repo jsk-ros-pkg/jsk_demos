f1cdffb5977a209423c75a094ab93a1778ec997d
Ryohei Ueda <garaemon@gmail.com>
2015-05-20 07:59:00 +0900

    [drc_task_common] Fixing tmux based launching

--- jsk_2015_06_hrp_drc/drc_task_common/operator_station.sh
+++ jsk_2015_06_hrp_drc/drc_task_common/operator_station.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-DRC_TASK_COMMON_LAUNCHES="ocs_executive.launch ui.launch ocs_rviz.launch ocs_misc.launch"
 CMDNAME=$(basename $0)
 FC_IP=localhost
 OCS_IP=localhost
-while getopts hf:o: OPT
+while getopts hf:o:ka OPT
 do
     case $OPT in
         "f") FC_IP="$OPTARG";;
         "o") OCS_IP="$OPTARG";;
-        "h") echo "Usage: $CMDNAME [-f FC_IP] [-o OCS_IP]";;
+        "k") tmux kill-session -t ocs; exit;;
+        "a") tmux a -t ocs; exit;;
+        "h") echo "Usage: $CMDNAME [-f FC_IP] [-o OCS_IP]"; exit;;
     esac
 done
 
@@ -22,26 +23,17 @@ tmux-newwindow() {
     fi
 }
 
-if [ $# -gt 0 ]; then
-    if [ $1 = "--kill" -o $1 = "k" ]; then
-        tmux kill-session -t ocs
-        exit 0
-    elif [ $1 = "--attach" -o $1 = "a" ]; then
-        tmux a -t ocs
-        exit 0
-    fi
-fi
-
 if `tmux has-session -t ocs`; then
     echo -e "\e[1;33msession named ocs already exists.\e[m"
+    exit 1
 else
     echo -e "\e[1;34mcreate new session named ocs.\e[m"
     tmux new-session -d -s ocs -n tmp
 fi
 
-for l in $DRC_TASK_COMMON_LAUNCHES
-do
-    tmux-newwindow $(basename $l .launch)_ocs "roslaunch drc_task_common $l"
-done
-tmux-newwindow operator_station_com_ocs "roslaunch drc_com_common operator_station_com.launch FC_IP:=${FC_IP} OCS_IP:=${OCS_IP}"
+tmux-newwindow executive "roslaunch drc_task_common ocs_executive.launch"
+tmux-newwindow ui "roslaunch drc_task_common ui.launch"
+tmux-newwindow rviz "roslaunch drc_task_common ocs_rviz.launch"
+tmux-newwindow misc "roslaunch drc_task_common ocs_misc.launch"
+tmux-newwindow com "roslaunch drc_com_common operator_station_com.launch FC_IP:=${FC_IP} OCS_IP:=${OCS_IP}"
 tmux send-keys -t ocs:tmp "exit" C-m
