d84cc099055800623f0395ba9eeb2028880ab58d
iory <ab.ioryz@gmail.com>
2015-06-04 06:26:39 +0900

    modified check node names and topic names

--- jsk_2015_06_hrp_drc/drc_task_common/scripts/check_sanity_ocs.py
+++ jsk_2015_06_hrp_drc/drc_task_common/scripts/check_sanity_ocs.py
@@ -12,34 +12,85 @@ from jsk_tools.sanity_lib import (okMessage, errorMessage, warnMessage, indexMes
 
 from std_msgs.msg import Time
 
-check_ocs_nodes = ["chesk_sanity_fc",
-                   "dammy_tf_broadcaster",
-                   "rviz_status",
-                   "ocs_dynamic_reconfigure",
-                   "button_checker",
-                   "stair_marker",
-                   'calc_box_plane',
-                   "spacenav_client",
-                   "robot_idle_watch",
-                   "time_update_text",
-                   'interpret_object',
-                   "filter_bbox_position",
-                   'b_control_client',
-                   'calc_drive_tf',
-                   "offset_bbox_from_plane",
-                   "static_transform_bounding_box_array",
-                   'wrench_fft',
-                   'joy_to_twist',
-                   "fc_dynamic_reconfigure",
-                   'fft_data',
-                   'drill_button_publisher',
-                   "ar_pose_to_pose",
-                   'fft_data',
-                   'polygon_to_centroid',
-                   "CarPathVisualizer",
-                   "magnetometer_to_direction",
-                   "SteeringAngleMarker",
-                   "google_translation_talk",
+check_ocs_nodes = ["/b_control_client",
+                   "/drill_button_publisher",
+                   "/dynamic_tf_publisher_for_urdf_marker",
+                   "/footstep_marker",
+                   "/footstep_overlay_text",
+                   "/footstep_planner",
+                   "/footstep_refinement",
+                   "/highspeed_receiver",
+                   "/highspeed_time_text",
+                   "/hrp2jsknts_ik_server",
+                   "/image_rect/rqt_image_view2_engine",
+                   "/image_rect/scale_background",
+                   "/image_rect/scale_foreground",
+                   "/image_rect/scale_line",
+                   "/image_rect/scale_poly",
+                   "/image_rect/scale_screenractangle",
+                   "/joint_states_storage",
+                   "/joy_to_twist",
+                   "/jsk_model_marker_interface",
+                   "/low_speed_basic_time_text",
+                   "/low_speed_time_text",
+                   "/midi_config_player",
+                   "/move_marker_with_point",
+                   "/ocs/accel_throttle",
+                   "/ocs/brake_throttle",
+                   "/ocs/dynamic_tf_publisher",
+                   "/ocs/env_server",
+                   "/ocs/ground_cloud",
+                   "/ocs/ground_extraction",
+                   "/ocs/ground_polygon_publisher",
+                   "/ocs/handle_throttle",
+                   "/ocs/joint_states_buffer_client",
+                   "/ocs/joy_driver",
+                   "/ocs/joy_manager",
+                   "/ocs/locomotion_manager",
+                   "/ocs/neck_p_throttle",
+                   "/ocs/neck_y_throttle",
+                   "/ocs/non_ground_points",
+                   "/ocs/obstacle_cloud",
+                   "/ocs/ocs_robot_state_publisher",
+                   "/ocs/plane_concatenator",
+                   "/ocs/plane_estimation",
+                   "/ocs/vehicle_ui",
+                   "/ocs_basic_info_publisher",
+                   "/ocs_dynamic_reconfigure",
+                   "/ocs_executive",
+                   "/ocs_from_fc_basic_low_speed",
+                   "/ocs_from_fc_eus",
+                   "/ocs_from_fc_low_speed",
+                   "/ocs_from_fc_vehicle",
+                   "/ocs_to_fc_eus",
+                   "/ocs_to_fc_low_speed",
+                   "/ocs_to_fc_reconfigure",
+                   "/ocs_to_fc_vehicle",
+                   "/operator_rviz",
+                   "/panorama_view/rqt_panorama_image_view2_engine",
+                   "/panorama_view/scale_background",
+                   "/panorama_view/scale_foreground",
+                   "/panorama_view/scale_line",
+                   "/panorama_view/scale_poly",
+                   "/panorama_view/scale_screenractangle",
+                   "/request_ik_from_marker",
+                   "/rosout",
+                   "/rqt_image_view_button",
+                   "/rviz_menu_server",
+                   "/rviz_status",
+                   "/spacenav",
+                   "/spacenav_client",
+                   "/stair_marker",
+                   "/stat_visualization/motor_states_temperature_decomposer",
+                   "/stat_visualization/robot_name_publisher",
+                   "/stat_visualization/temperature_name_publisher",
+                   "/state_image_publisher",
+                   "/state_viewer",
+                   "/task_state_manager",
+                   "/transformable_interactive_server",
+                   "/urdf_control_marker",
+                   "/vehicle_ocs_executive",
+                   "/walk_to_object",
                    ]
 
 def main():
@@ -49,59 +100,41 @@ def main():
     checkROSMasterCLOSE_WAIT(host)
 
     indexMessage("Check Nodes in OCS")
-    checkNodeState("/fc_to_ocs_basic_low_speed", True)
-    checkNodeState("/fc_to_ocs_eus", True)
-    checkNodeState("/fc_to_ocs_low_speed", True)
-    checkNodeState("/fc_to_ocs_vehicle", True)
-    checkNodeState("/fc_from_ocs_eus", True)
-    checkNodeState("/fc_from_ocs_low_speed", True)
-    checkNodeState("/fc_from_ocs_vehicle", True)
-    checkNodeState("/fc_from_ocs_reconfigure", True)
-    checkNodeState("/highspeed_streamer", True)
+    # check Node State
+    for node in check_ocs_nodes:
+        checkNodeState(node, needed=True)
 
-    indexMessage("Check Nodes in OCS")
-
-    #Check Input Device
-    checkTopicIsPublished("/b_control/joy", None,
-                          "MIDI Controller seems to work",
-                          "MIDI Controller doesn't publish. Does you connect?")
+    indexMessage("Check Input Device")
+    # check Input Device
+    checkNodeState('/midi_config_player', needed=True)
     checkTopicIsPublished("/ocs/joy", None,
                           "XBox Controller seems to work",
                           "XBox Controller doesn't publish. Did you connect?")
 
     checkTopicIsPublished(
-        "/fc_from_ocs_eus/last_publish_output_time",
+        "/ocs_dynamic_reconfigure/parameter_descriptions",
         Time,
         "[ocs] Silverhammer lowspeed protocol is working",
         "[ocs] Silverhammer lowspeed protocol is not working",
         5,
-        [["/fc_from_ocs_eus/last_received_time", Time],
-         ["/fc_from_ocs_eus/output", Time],
-         ["/fc_from_ocs_low_speed/last_publish_output_time", Time],
-         ["/fc_from_ocs_low_speed/last_received_time", Time],
-         ["/fc_from_ocs_low_speed/output", Time],
-         ["/fc_from_ocs_reconfigure/last_publish_output_time", Time],
-         ["/fc_from_ocs_reconfigure/last_received_time", Time],
-         ["/fc_from_ocs_reconfigure/output", Time],
-         ["/fc_from_ocs_vehicle/last_publish_output_time", Time],
-         ["/fc_from_ocs_vehicle/last_received_time", Time],
-         ["/fc_from_ocs_vehicle/output", Time],
-         ["/fc_to_ocs_basic_low_speed/input", Time],
-         ["/fc_to_ocs_basic_low_speed/last_input_received_time", Time],
-         ["/fc_to_ocs_basic_low_speed/last_send_time", Time],
-         ["/fc_to_ocs_eus/input", Time],
-         ["/fc_to_ocs_eus/last_input_received_time", Time],
-         ["/fc_to_ocs_eus/last_send_time", Time],
-         ["/fc_to_ocs_low_speed/input", Time],
-         ["/fc_to_ocs_low_speed/last_input_received_time", Time],
-         ["/fc_to_ocs_low_speed/last_send_time", Time],
-         ["/fc_to_ocs_vehicle/input", Time],
-         ["/fc_to_ocs_vehicle/last_input_received_time", Time],
-         ["/fc_to_ocs_vehicle/last_send_time", Time]])
-
-    # check Node State
-    for node in check_ocs_nodes:
-        checkNodeState(node, needed=True)
+        [
+        ["/ocs_from_fc_basic_low_speed/last_publish_output_time", Time],
+        ["/ocs_from_fc_basic_low_speed/last_received_time", Time],
+        ["/ocs_from_fc_eus/last_publish_output_time", Time],
+        ["/ocs_from_fc_eus/last_received_time", Time],
+        ["/ocs_from_fc_low_speed/last_publish_output_time", Time],
+        ["/ocs_from_fc_low_speed/last_received_time", Time],
+        ["/ocs_from_fc_vehicle/last_publish_output_time", Time],
+        ["/ocs_from_fc_vehicle/last_received_time", Time],
+        ["/ocs_to_fc_eus/last_input_received_time", Time],
+        ["/ocs_to_fc_eus/last_send_time", Time],
+        ["/ocs_to_fc_low_speed/last_input_received_time", Time],
+        ["/ocs_to_fc_low_speed/last_send_time", Time],
+        ["/ocs_to_fc_reconfigure/last_input_received_time", Time],
+        ["/ocs_to_fc_reconfigure/last_send_time", Time],
+        ["/ocs_to_fc_vehicle/last_input_received_time", Time],
+        ["/ocs_to_fc_vehicle/last_send_time", Time],
+        ])
 
 if __name__ == "__main__":
     main()
