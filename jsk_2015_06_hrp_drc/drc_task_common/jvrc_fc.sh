f729c2a36a1c40ab4e4ba0664e5d6c148e969009
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2015-09-25 18:05:34 +0900

    use pr2_navigation_self_filter

--- jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
+++ jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
@@ -4,6 +4,6 @@ source `dirname ${0}`/fc_tmux_init.sh
 
 tmux-newwindow executive "sleep 1; roslaunch drc_task_common fc_executive.launch fc:=false"
 tmux-newwindow lookat "sleep 1; roslaunch drc_task_common lookat.launch"
-tmux-newwindow locomotion "sleep 5; roslaunch drc_task_common locomotion.launch INPUT_POINT_CLOUD:=/multisense/resize_1_2/points"
+tmux-newwindow locomotion "sleep 5; roslaunch drc_task_common locomotion.launch INPUT_POINT_CLOUD:=/multisense/resize_1_2/points RUN_SELF_FILTER:=true SELF_FILTER_PARAM:=$(rospack find jaxon_ros_bridge)/config/jaxon_self_filter.yaml"
 tmux send-keys -t fc:tmp "exit" C-m
 tmux a -t fc
