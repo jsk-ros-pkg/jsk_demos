e9b1b03d338ae65f5d183f4bd0f67af4d580572a
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2015-09-29 21:29:44 +0900

    add multisense launch

--- jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
+++ jsk_2015_06_hrp_drc/drc_task_common/jvrc_fc.sh
@@ -2,8 +2,10 @@
 
 source `dirname ${0}`/fc_tmux_init.sh
 
-tmux-newwindow executive "sleep 1; roslaunch drc_task_common fc_executive.launch fc:=false"
-tmux-newwindow lookat "sleep 1; roslaunch drc_task_common lookat.launch"
-tmux-newwindow locomotion "sleep 5; roslaunch drc_task_common locomotion.launch INPUT_POINT_CLOUD:=/multisense/resize_1_2/points"
+tmux-newwindow executive "roslaunch drc_task_common fc_executive.launch fc:=false"
+tmux-newwindow lookat "roslaunch drc_task_common lookat.launch"
+tmux-newwindow locomotion "roslaunch drc_task_common locomotion.launch INPUT_POINT_CLOUD:=/multisense/resize_1_2/points"
+tmux-newwindow multisense_local "roslaunch jaxon_ros_bridge/launch/jaxon_multisense_local.launch"
+tmux-newwindow multisense_remote "roslaunch hrpsys_ros_bridge/launch/jaxon_jvrc_multisense.launch"
 tmux send-keys -t fc:tmp "exit" C-m
 tmux a -t fc
