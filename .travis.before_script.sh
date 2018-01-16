b2e596710365286fc187990c11bc0e21bae3de97
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-01-16 10:36:12 +0900

    use TEST_ROBOT for env

--- .travis.before_script.sh
+++ .travis.before_script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 install_robot_rosinstall() {
-  if [ "$ROBOT" = "pr2" ]; then
+  if [ "$TEST_ROBOT" = "pr2" ]; then
     ROSINSTALL_URL="https://raw.githubusercontent.com/jsk-ros-pkg/jsk_robot/master/jsk_pr2_robot/jsk_pr2_startup/jsk_pr2.rosinstall"
-  elif [ "$ROBOT" = "fetch" ]; then
+  elif [ "$TEST_ROBOT" = "fetch" ]; then
     ROSINSTALL_URL="https://raw.githubusercontent.com/jsk-ros-pkg/jsk_robot/master/jsk_fetch_robot/jsk_fetch.rosinstall"
   fi
 
   if [ "$ROSINSTALL_URL" != "" ]; then
+    echo "Appending .rosinstall for robot $TEST_ROBOT"
     wget "$ROSINSTALL_URL" -O - | wstool merge -t ~/ros/ws_$REPOSITORY_NAME/src -
   else
+    echo "Appending default .rosinstall"
     wstool merge -t ~/ros/ws_$REPOSITORY_NAME/src $CI_SOURCE_PATH/.travis.rosinstall.default
   fi
 }
