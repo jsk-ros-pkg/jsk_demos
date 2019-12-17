0dff618109aa9ae72523dba4b95433717352034a
Yuto Uchimi <momosama1072@gmail.com>
2019-12-17 23:34:57 +0900

    Use no_sync in find_human_in_mirror.py

--- elevator_move_base_pr2/node_scripts/find_human_in_mirror.py
+++ elevator_move_base_pr2/node_scripts/find_human_in_mirror.py
@@ -17,31 +17,49 @@ class FindHumanInMirror(ConnectionBasedTransport):
         self.mirror_label = rospy.get_param('~mirror_label', 1)
         self.score_thre = rospy.get_param('~score_threshold', 0.5)
         self.bridge = cv_bridge.CvBridge()
+        self.no_sync = rospy.get_param('~no_sync', False)
+        self.approximate_sync = rospy.get_param('~approximate_sync', False)
+        self.ppl_msg = None
         self.pub_inside = self.advertise(
             '~output/inside_mirror', BoolStamped, queue_size=1)
         self.pub_outside = self.advertise(
             '~output/outside_mirror', BoolStamped, queue_size=1)
 
     def subscribe(self):
-        queue_size = rospy.get_param('~queue_size', 10)
-        sub_people_pose = message_filters.Subscriber(
-            '~input/people_pose_array', PeoplePoseArray, queue_size=1)
-        sub_label = message_filters.Subscriber(
-            '~input/label', Image,
-            queue_size=1, buff_size=2**24)
-        self.subs = [sub_people_pose, sub_label]
-        if rospy.get_param('~approximate_sync', False):
-            slop = rospy.get_param('~slop', 0.1)
-            sync = message_filters.ApproximateTimeSynchronizer(
-                fs=self.subs, queue_size=queue_size, slop=slop)
+        if self.no_sync:
+            sub_people_pose = rospy.Subscriber(
+                '~input/people_pose_array', PeoplePoseArray,
+                self._people_pose_cb, queue_size=1)
+            sub_label = rospy.Subscriber(
+                '~input/label', Image, self._label_cb, queue_size=1)
+            self.subs = [sub_people_pose, sub_label]
         else:
-            sync = message_filters.TimeSynchronizer(
-                fs=self.subs, queue_size=queue_size)
-        sync.registerCallback(self._cb)
+            sub_people_pose = message_filters.Subscriber(
+                '~input/people_pose_array', PeoplePoseArray, queue_size=1)
+            sub_label = message_filters.Subscriber(
+                '~input/label', Image,
+                queue_size=1, buff_size=2**24)
+            self.subs = [sub_people_pose, sub_label]
+            if self.approximate_sync:
+                queue_size = rospy.get_param('~queue_size', 10)
+                slop = rospy.get_param('~slop', 0.1)
+                sync = message_filters.ApproximateTimeSynchronizer(
+                    fs=self.subs, queue_size=queue_size, slop=slop)
+            else:
+                sync = message_filters.TimeSynchronizer(
+                    fs=self.subs, queue_size=queue_size)
+            sync.registerCallback(self._sync_cb)
 
     def unsubscribe(self):
         for sub in self.subs:
             sub.unregister()
+
+    def _people_pose_cb(self, ppl_msg):
+        self.ppl_msg = ppl_msg
+
+    def _label_cb(self, lbl_msg):
+        if self.ppl_msg is not None:
+            return self._cb(self.ppl_msg, lbl_msg)
 
     def _cb(self, ppl_msg, lbl_msg):
         rospy.loginfo('Start callback...')
