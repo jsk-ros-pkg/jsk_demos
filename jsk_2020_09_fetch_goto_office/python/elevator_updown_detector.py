933909d397e7ee528ad27a81a0346128fde93521
obinata <mqcmd196@hotmail.co.jp>
2020-10-08 19:39:24 +0900

    add elevator updown detector

--- jsk_2020_09_fetch_goto_office/python/elevator_updown_detector.py
+++ jsk_2020_09_fetch_goto_office/python/elevator_updown_detector.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Imu
+from std_msgs import Float64, String
 
 class UpDownDetectPublisher:
     def __init__(self):
         rospy.init_node('updown_detector', anonymous=True)
-        self.sub = rospy.Subscriber('imu', Imu, self.callback)
+        self.sub = rospy.Subscriber('/imu_filtered/z', Float64, self.callback)
+        self.pub = rospy.Publisher('/updown_state', String, queue_size=1)
+        self.
 
     def callback(self, data):
-        rospy.loginfo("imu.z is = {}".format(data.linear_acceleration.z))
+        self.publisher(data)
+        
+    def publisher(self, data):
+        state = updownstate_detector(data)
+        self.pub.publish(state)
+
+    def updownstate_detector(self, data, acceleration_th):
+        
+        
 
 if __name__ == '__main__':
-
     try:
         node = UpDownDetectPublisher()
         while not rospy.is_shutdown():
