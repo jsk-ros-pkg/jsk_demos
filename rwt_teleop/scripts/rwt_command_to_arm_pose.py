5e99c371207be3d527aeb32b4ddc183e0f5c69b5
ishiguroJSK <ishiguro@jsk.imi.i.u-tokyo.ac.jp>
2020-12-04 21:20:53 +0900

    up

--- rwt_teleop/scripts/rwt_command_to_arm_pose.py
+++ rwt_teleop/scripts/rwt_command_to_arm_pose.py
@@ -9,35 +9,36 @@ from geometry_msgs.msg import *
 from pr2_controllers_msgs.msg import Pr2GripperCommandActionGoal
 
 class RwtCommandToArmPose():
-  LR          = ["l", "r"]
-  sgns        = { "l" : 1, "r" : -1 }
+  LR   = ["l", "r"]
+  sgns = { "l" : 1, "r" : -1 }
 
   def __init__(self):
     rospy.init_node('RwtCommandToArmPose')
-    self.ee_pose         = {}
-    self.ep_pub          = {}
-    self.g_pub           = {}
-    self.current_lr_mode = "l"
-    self.pan = 0
-    self.tilt = 0
-
+    self.ee_pose      = {}
+    self.ep_pub       = {}
+    self.g_pub        = {}
     self.tfl = tf.TransformListener()
     self.cmd_str_sub = rospy.Subscriber("/rwt_command_string", String, self.cmd_str_cb)
     self.sub = rospy.Subscriber("/pointcloud_screenpoint_nodelet/output_point", PointStamped, self.click_cb)
 
     for lr in self.LR:
-      self.ee_pose[lr] = PoseStamped()
-      self.ee_pose[lr].header.frame_id  = "base_link"
+      self.ee_pose[lr] = PoseStamped(header=Header(frame_id="base_link"))
       self.ee_pose[lr].pose.position    = Point(0.5, self.sgns[lr] * 0.3, 1.0)
       self.ee_pose[lr].pose.orientation = Quaternion(0,0,0,1)
       self.ep_pub[lr]  = rospy.Publisher('/master_'+lr+'arm_pose', PoseStamped, queue_size=1)
       self.g_pub[lr]   = rospy.Publisher('/'+lr+'_gripper_controller/gripper_action/goal', Pr2GripperCommandActionGoal, queue_size=1)
-    
+  
+    self.head_pose     = PoseStamped(header=Header(frame_id="base_link"))
     self.head_pub      = rospy.Publisher('/master_head_pose', PoseStamped, queue_size=1)
-    self.head_pose     = PoseStamped()
+    self.lr_for_click  = "l"
+    self.lr_mode_pub   = rospy.Publisher('/rwt_current_state', String, queue_size=1)
 
     rospy.loginfo("start main loop")
-    rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+      self.lr_mode_pub.publish(self.lr_for_click)
+      rate.sleep()
+
 
   def click_cb(self, msg):
     try:
@@ -47,11 +48,9 @@ class RwtCommandToArmPose():
       rospy.logerr(e)
       return
 
-    self.ee_pose[self.current_lr_mode].header.frame_id = "base_link"
-    self.ee_pose[self.current_lr_mode].header.stamp    = msg.header.stamp
-    self.ee_pose[self.current_lr_mode].pose.position   = Point( pos[0], pos[1], pos[2])
-    # self.clk_tgt_pos[self.current_lr_mode].pose.orientation = Quaternion(0,0,0,1)
-    self.ep_pub[self.current_lr_mode].publish(self.ee_pose[self.current_lr_mode])
+    self.ee_pose[self.lr_for_click].header.stamp  = msg.header.stamp
+    self.ee_pose[self.lr_for_click].pose.position = Point( pos[0], pos[1], pos[2])
+    self.ep_pub[self.lr_for_click].publish(self.ee_pose[self.lr_for_click])
 
 
   def cmd_str_cb(self, msg):
@@ -60,7 +59,7 @@ class RwtCommandToArmPose():
     if len(str_l) == 2:
       tgt, cmd = str_l # l_open
     elif len(str_l) == 3:
-      tgt, cmd, val = str_l # l_turn_90
+      tgt, cmd, val = str_l # l_turn_l
     else:
       print "something wrong"
       return
@@ -68,43 +67,55 @@ class RwtCommandToArmPose():
     if cmd == "look":
       q = self.head_pose.pose.orientation
       r, p, y = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
-      
-      if tgt == "l": y += math.pi / 6
-      if tgt == "r": y -= math.pi / 6
-      if tgt == "u": p -= math.pi / 6
-      if tgt == "d": p += math.pi / 6
-      if tgt == "c": r, p, y = 0, 0, 0
-      
+      delta = math.radians(30)
+      if val == "l": y += delta
+      if val == "r": y -= delta
+      if val == "u": p -= delta
+      if val == "d": p += delta
+      if val == "c": r, p, y = 0, 0, 0
       p = min(max(p, math.radians(-80)), math.radians(80))
       y = min(max(y, math.radians(-170)), math.radians(170))
       q = tf.transformations.quaternion_from_euler(r,p,y)
-      self.head_pose.header.frame_id  = "base_link"
-      self.head_pose.header.stamp     = rospy.Time.now()
       self.head_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
+      self.head_pose.header.stamp     = rospy.Time.now()
       self.head_pub.publish(self.head_pose)
 
     lr = tgt
       
-    if cmd == "mode":
-      self.current_lr_mode = lr
+    if cmd == "clickteleopmode":
+      self.lr_for_click = lr
 
-    if cmd in ["open", "close"]:
+    if cmd == "gripper":
       gripper_cmd = Pr2GripperCommandActionGoal()
-      gripper_cmd.goal.command.position = ( 0.0 if cmd == "close" else 0.1)
+      gripper_cmd.goal.command.position = ( 0.1 if val == "on" else 0.0)
       gripper_cmd.goal.command.max_effort = 75
       self.g_pub[lr].publish(gripper_cmd)
 
+    if cmd == "jog":
+      delta = 0.05
+      if val == "f": self.ee_pose[lr].pose.position.x += delta
+      if val == "b": self.ee_pose[lr].pose.position.x -= delta
+      if val == "l": self.ee_pose[lr].pose.position.y += delta
+      if val == "r": self.ee_pose[lr].pose.position.y -= delta
+      if val == "u": self.ee_pose[lr].pose.position.z += delta
+      if val == "d": self.ee_pose[lr].pose.position.z -= delta
+      self.ee_pose[lr].header.stamp  = rospy.Time.now()
+      self.ep_pub[lr].publish(self.ee_pose[lr])
+
     if cmd == "pull":
-      tmp = copy.deepcopy(self.ee_pose[lr])
-      tmp.pose.position.x -= 0.3
-      tmp.pose.position.z -= 0.2
-      self.ep_pub[lr].publish(tmp)
+      self.ee_pose[lr].pose.position.x -= 0.3
+      self.ee_pose[lr].pose.position.z -= 0.2
+      self.ee_pose[lr].header.stamp  = rospy.Time.now()
+      self.ep_pub[lr].publish(self.ee_pose[lr])
 
     if cmd == "turn":
       q_org = self.ee_pose[lr].pose.orientation
-      q_rel = tf.transformations.quaternion_from_euler(math.pi/2,0,0)
+      q_rel = tf.transformations.quaternion_from_euler( math.radians( 30 if val == "r" else -30 ) ,0 ,0)
       q_new = tf.transformations.quaternion_multiply(q_rel, [q_org.x, q_org.y, q_org.z, q_org.w])
       self.ee_pose[lr].pose.orientation = Quaternion(q_new[0],q_new[1],q_new[2],q_new[3])
+      self.ee_pose[lr].header.stamp  = rospy.Time.now()
+      self.ep_pub[lr].publish(self.ee_pose[lr])
+
 
 if __name__ == '__main__':
   inst = RwtCommandToArmPose()
