5ae67ac2f50603d9dbcb2b90e9bf5a2fdb2f7cd0
Koki Shinjo <sktometometo@gmail.com>
2021-05-09 13:56:45 +0900

    [spot_person_leader] update map

--- spot_person_leader/scripts/download_autowalk_data.py
+++ spot_person_leader/scripts/download_autowalk_data.py
@@ -8,17 +8,17 @@ def main():
 
     download_data(
         pkg_name=PKG,
-        path='autowalk/eng2_73b2_to_81c1_night.walk.tar.gz',
-        url='https://drive.google.com/uc?id=1PXgMsmN3uKPG3YAap9vdfrBduEiLgAl8',
-        md5='a79dd142013b12c0babb4400be583739',
+        path='autowalk/eng2_73B2_to_81C1.walk.tar.gz',
+        url='https://drive.google.com/uc?id=1IDTCP7n4LCowizW3mFQvTOQtE4qOH9tx',
+        md5='65f09629c0ac2aa21df6f179c9875bd0',
         extract=True
     )
 
     download_data(
         pkg_name=PKG,
-        path='autowalk/eng2_73b2_to_7FElevator.walk.tar.gz',
+        path='autowalk/eng2_73B2_to_7FElevator.walk.tar.gz',
         url='https://drive.google.com/uc?id=1O8o6voq2v8WenfaUYcmpSU-IwJSsXW5_',
-        md5='160e2052d51b9a2f56cf9cf1788b18c2',
+        md5='ecd4d8dc043995f7675a59fce950676b',
         extract=True
     )
 
