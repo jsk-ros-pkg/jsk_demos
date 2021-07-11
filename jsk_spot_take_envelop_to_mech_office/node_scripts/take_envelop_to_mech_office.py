13dc10b5a8a5eea4046575768de6be9f128b561c
Koki Shinjo <sktometometo@gmail.com>
2021-07-11 20:09:48 +0900

    [jsk_spot_take_envelop_to_mech_office] update

--- jsk_spot_take_envelop_to_mech_office/node_scripts/take_envelop_to_mech_office.py
+++ jsk_spot_take_envelop_to_mech_office/node_scripts/take_envelop_to_mech_office.py
@@ -8,16 +8,51 @@ import rospy
 from spot_behavior_manager_msgs.msg import LeadPersonAction, LeadPersonGoal
 from std_msgs.msg import String
 import sys
+import smtplib
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import formatdate
 
 
-def sendResultMail():
+def sendResultMail(from_address,to_address):
 
-    last_node_id = rospy.wait_for_message('')
+    subject = 'Result of Mech Office Demo'
+    text = 'I have succeeded to take envelops to Mech Office.'
+
+    msg = MIMEText(text.encode('utf-8'),'plain','utf-8')
+    msg['Subject'] = Header(subject,'utf-8')
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Date'] = formatdate(localtime=True)
+
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(from_address,to_address,msg.as_string())
+    smtp.close()
+
+    rospy.loginfo('Sent result mail')
 
     return True
 
 
-def sendRescueMail():
+def sendRescueMail(from_address,to_address):
+
+    last_node_id = rospy.wait_for_message('/spot_behavior_manager_demo/current_node_id').data
+
+    subject = '[Important] Please rescue me by Spot'
+    text = 'I have got stuck around node {}.\n'.format(last_node_id) + \
+           'Please rescue me!'
+
+    msg = MIMEText(text.encode('utf-8'),'plain','utf-8')
+    msg['Subject'] = Header(subject,'utf-8')
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Date'] = formatdate(localtime=True)
+
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(from_address,to_address,msg.as_string())
+    smtp.close()
+
+    rospy.loginfo('Sent rescue mail')
 
     return True
 
@@ -37,8 +72,10 @@ class Demo:
         self.node_id_mech_office = rospy.get_param(
             '~node_id_mech_office', 'eng2_Mech_Office')
         self.num_max_retry = rospy.get_param('~num_max_retry', 3)
-        self.mailaddress = rospy.get_param(
-            '~mailaddress', 'spot@jsk.imi.i.u-tokyo.ac.jp')
+        self.from_address = rospy.get_param(
+            '~from_address', 'shinjo@jsk.imi.i.u-tokyo.ac.jp')
+        self.to_address = rospy.get_param(
+            '~to_address', 'spot@jsk.imi.i.u-tokyo.ac.jp')
 
         self.actionclient = actionlib.SimpleActionClient(
             '/spot_behavior_manager_demo/lead_person', LeadPersonAction)
@@ -68,7 +105,7 @@ class Demo:
                     self.exit(1)
 
         rospy.loginfo('I am back to 73B2.')
-        sendResultMail()
+        sendResultMail(self.from_address,self.to_address)
         sys.exit(1)
 
     def run(self):
@@ -108,7 +145,7 @@ class Demo:
                     self.exit()
 
         rospy.loginfo('I am back to 73B2.')
-        sendResultMail()
+        sendResultMail(self.from_address,self.to_address)
 
 
 if __name__ == '__main__':
