aa06d7649129df14b4073473261d0d3adb78f66d
Koki Shinjo <sktometometo@gmail.com>
2021-10-06 22:31:56 +0900

    [spot_person_lead_demo] bugfix

--- spot_person_leader/spot_person_lead_demo/node_scripts/person_lead_demo.py
+++ spot_person_leader/spot_person_lead_demo/node_scripts/person_lead_demo.py
@@ -21,26 +21,26 @@ def main():
     sound_client = SoundClient(sound_action='/robotsound_jp', sound_topic='/robotsound_jp')
     action_server_lead_to = actionlib.SimpleActionClient('/spot_behavior_manager_demo/execute_behaviors', LeadPersonAction)
 
-    self.node_list = rospy.get_param('~nodes', {})
+    node_list = rospy.get_param('~nodes', {})
 
     while not rospy.is_shutdown():
 
         rospy.loginfo('Asking package information')
         while not rospy.is_shutdown():
-            self.sound_client.say('行き先を教えてください。', blocking=True)
-            recogntion_result = self.speech_recognition_client.recognize()
+            sound_client.say('行き先を教えてください。', blocking=True)
+            recogntion_result = speech_recognition_client.recognize()
             if len(recogntion_result.transcript) == 0:
                 rospy.logerr('No matching node found from spoken \'{}\''.format(recogntion_result))
-                self.sound_client.say('行き先がわかりませんでした', blocking=True)
+                sound_client.say('行き先がわかりませんでした', blocking=True)
                 continue
             recognized_destination = recogntion_result.transcript[0]
             target_node_candidates = {}
-            for node_id, value in self.node_list.items():
+            for node_id, value in node_list.items():
                 if value.has_key('name_jp') and value['name_jp'].encode('utf-8') == recognized_destination:
                     target_node_candidates[node_id] = value
             if len(target_node_candidates) == 0:
                 rospy.logerr('No matching node found from spoken \'{}\''.format(recogntion_result))
-                self.sound_client.say('行き先がわかりませんでした', blocking=True)
+                sound_client.say('行き先がわかりませんでした', blocking=True)
             else:
                 rospy.loginfo('target_node_candidates: {}'.format(target_node_candidates))
                 break
