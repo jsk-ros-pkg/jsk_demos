e6f940642d70fbbc910b9b2d77ccaa79e8263a2e
Koki Shinjo <sktometometo@gmail.com>
2021-10-06 22:22:11 +0900

    [spot_person_lead_demo] add interaction node

---
+++ spot_person_leader/spot_person_lead_demo/node_scripts/person_lead_demo.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import actionlib
+import rospy
+
+from spot_behavior_manager_msgs.msg import LeadPersonAction, LeadPersonGoal
+
+from sound_play.libsoundplay import SoundClient
+from ros_speech_recognition import SpeechRecognitionClient
+
+import smach
+import smach_ros
+
+
+def main():
+
+    rospy.init_node('person_lead_demo')
+
+    speech_recongition_client = SpeechRecognitionClient()
+    sound_client = SoundClient(sound_action='/robotsound_jp', sound_topic='/robotsound_jp')
+    action_server_lead_to = actionlib.SimpleActionClient('/spot_behavior_manager_demo/execute_behaviors', LeadPersonAction)
+
+    self.node_list = rospy.get_param('~nodes', {})
+
+    while not rospy.is_shutdown():
+
+        rospy.loginfo('Asking package information')
+        while not rospy.is_shutdown():
+            self.sound_client.say('行き先を教えてください。', blocking=True)
+            recogntion_result = self.speech_recognition_client.recognize()
+            if len(recogntion_result.transcript) == 0:
+                rospy.logerr('No matching node found from spoken \'{}\''.format(recogntion_result))
+                self.sound_client.say('行き先がわかりませんでした', blocking=True)
+                continue
+            recognized_destination = recogntion_result.transcript[0]
+            target_node_candidates = {}
+            for node_id, value in self.node_list.items():
+                if value.has_key('name_jp') and value['name_jp'].encode('utf-8') == recognized_destination:
+                    target_node_candidates[node_id] = value
+            if len(target_node_candidates) == 0:
+                rospy.logerr('No matching node found from spoken \'{}\''.format(recogntion_result))
+                self.sound_client.say('行き先がわかりませんでした', blocking=True)
+            else:
+                rospy.loginfo('target_node_candidates: {}'.format(target_node_candidates))
+                break
+
+        target_node_id = target_node_candidates.keys()[0]
+        target_node_name_jp = target_node_candidates[0]['name_jp']
+
+        rospy.loginfo('leading person to {}'.format())
+        action_server_lead_to.send_goal_and_wait(LeadPersonGoal(target_node_id=target_node_id))
+
+
+if __name__ == '__main__':
+    main()
