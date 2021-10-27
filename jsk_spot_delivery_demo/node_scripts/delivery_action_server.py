ce615f6332ee6b1723604f0f539897d7d5e4d6df
Koki Shinjo <sktometometo@gmail.com>
2021-10-27 16:41:59 +0900

    [jsk_spot_delivery_demo] update demo

--- jsk_spot_delivery_demo/node_scripts/delivery_action_server.py
+++ jsk_spot_delivery_demo/node_scripts/delivery_action_server.py
@@ -66,19 +66,19 @@ class DeliveryActionServer:
 
     def callback_pickup_package(self, goal):
 
+        result = PickupPackageResult()
         timeout_deadline = rospy.Time.now() + goal.timeout
 
         rospy.loginfo('Asking package information')
         success = False
         while not rospy.is_shutdown() and rospy.Time.now() < timeout_deadline:
             self.sound_client.say('配達先を教えてください。', blocking=True)
-            recogntion_result = self.speech_recognition_client.recognize()
-            if len(recogntion_result.transcript) == 0:
-                rospy.logerr(
-                    'No matching node found from spoken \'{}\''.format(recogntion_result))
+            recognition_result = self.speech_recognition_client.recognize()
+            if len(recognition_result.transcript) == 0:
+                rospy.logerr('No matching node found from spoken \'{}\''.format(recognition_result))
                 self.sound_client.say('配達先がわかりませんでした', blocking=True)
                 continue
-            recognized_destination = recogntion_result.transcript[0]
+            recognized_destination = recognition_result.transcript[0]
             target_node_candidates = {}
             for node_id, value in self.node_list.items():
                 try:
@@ -95,56 +95,85 @@ class DeliveryActionServer:
                 except Exception as e:
                     rospy.logerr('Error: {}'.format(e))
             if len(target_node_candidates) == 0:
-                rospy.logerr(
-                    'No matching node found from spoken \'{}\''.format(recogntion_result))
+                rospy.logerr('No matching node found from spoken \'{}\''.format(recognition_result))
                 self.sound_client.say('配達先がわかりませんでした', blocking=True)
             else:
                 success = True
                 break
 
         if not success:
-            self.actionserver_pickup_package.set_aborted(
-                PickupPackageResult(False, 'Falied to recognize the destination from speech.'))
+            result.success = False
+            result.message = 'Falied to recognize the destination from speech.'
+            self.actionserver_pickup_package.set_aborted(result)
             return
         else:
             target_node_id = target_node_candidates.keys()[0]
             target_node_name_jp = self.node_list[target_node_id]['name_jp'].encode('utf-8')
             rospy.loginfo('target_node_id: {}'.format(target_node_id))
-            self.sound_client.say(
-                '{}ですね。わかりました。'.format(target_node_name_jp),blocking=True)
+            self.sound_client.say('{}ですね。わかりました。'.format(target_node_name_jp),blocking=True)
+
+
+        rospy.loginfo('Asking sender information')
+        success = False
+        while not rospy.is_shutdown() and rospy.Time.now() < timeout_deadline:
+            self.sound_client.say('送り主の名前を教えてください', blocking=True)
+            recognition_result = self.speech_recognition_client.recognize()
+            if len(recognition_result.transcript) == 0:
+                rospy.logerr('No spoken result: \'{}\''.format(recognition_result))
+                self.sound_client.say('聞き取れませんでした', blocking=True)
+                continue
+            recognized_name = recognition_result.transcript[0]
+            self.sound_client.say('{}さんですね',blocking=True)
+            success = True
+
+        if not success:
+            result.success = False
+            result.message = 'Falied to recognize sender name from speech.'
+            self.actionserver_pickup_package.set_aborted(result)
+            return
+
 
         rospy.loginfo('Waiting for package placed.')
         self.sound_client.say('荷物を置いてください', blocking=True)
-        success = self.wait_package_setting(
-            timeout_deadline - rospy.Time.now())
-        if success:
+        success = self.wait_package_setting(timeout_deadline - rospy.Time.now())
+        if not success:
+            rospy.logerr('Timeout for package placement')
+            result.success = False
+            result.message = 'Timeout for package placement.'
+            self.actionserver_pickup_package.set_aborted(result)
+        else:
             rospy.loginfo('Package placed')
             self.sound_client.say('荷物を確認しました', blocking=True)
-            self.actionserver_pickup_package.set_succeeded(
-                PickupPackageResult(True, target_node_id)
-            )
-        else:
-            rospy.logerr('Timeout')
-            self.actionserver_pickup_package.set_aborted(
-                PickupPackageResultoResult(False, 'Falied to recognize package.'))
+
+
+        result.success = True
+        result.message = 'Success'
+        result.task.target_node_id = target_node_id
+        result.task.package_content = ''
+        result.task.sender = sender_name
+        self.actionserver_pickup_package.set_succeeded(result)
+
 
     def callback_deliver_to(self, goal):
 
-        target_node_id = goal.target_node_id
+        task = goal.task
+        target_node_id = task.target_node_id
 
-        rospy.loginfo('move to {}'.format(target_node_id))
-        result = self.spot_ros_client.execute_behaviors(target_node_id)
+        rospy.loginfo('move to {}'.format(task.target_node_id))
+        result = self.spot_ros_client.execute_behaviors(task.target_node_id)
         rospy.logwarn('result: {}'.format(result))
         if not result.success:
-            rospy.logerr('Failed to reach {}'.format(target_node_id))
+            rospy.logerr('Failed to reach {}'.format(task.target_node_id))
             self.actionserver_deliver_to.set_aborted(
                 DeliverToResult(False, 'Falied to reach the destination.'))
             return
-        rospy.loginfo('reached {}'.format(target_node_id))
+        rospy.loginfo('reached {}'.format(task.target_node_id))
 
         while not rospy.is_shutdown():
             rospy.loginfo('Waiting for package picked.')
-            self.sound_client.say('荷物を受け取ってください', blocking=True)
+            self.sound_client.say(
+                    '{}さんから{}のお届けです、荷物を受け取ってください'.format(task.sender_name,task.package_content),
+                    blocking=True)
             if self.wait_package_setting(rospy.Duration(5)):
                 rospy.loginfo('Package picked')
                 break
