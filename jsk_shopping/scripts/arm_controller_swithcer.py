3dea3d5091176378a9f57c936ae1487b5add9921
Naoto Tsukamoto <naototukka0413@gmail.com>
2021-10-30 19:36:03 +0900

    fix indent to correct right format

--- jsk_shopping/scripts/arm_controller_swithcer.py
+++ jsk_shopping/scripts/arm_controller_swithcer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-# mainly copied from fetch_robots fetch_bringup/scripts/controller_reset.py
+# mainly copied from
+# fetch_robots fetch_bringup/scripts/controller_reset.py
+# https://github.com/fetchrobotics/fetch_robots/blob/d2923e06c9a84e70abb0ac9b89aefd6dfc7b9c2e/fetch_bringup/scripts/controller_reset.py NOQA
 
 import rospy
 import actionlib
 from actionlib_msgs.msg import GoalStatus
 from control_msgs.msg import GripperCommandAction, GripperCommandGoal
-from robot_controllers_msgs.msg import QueryControllerStatesAction,\
-                                        QueryControllerStatesGoal,\
-                                        ControllerState
+from robot_controllers_msgs.msg import QueryControllerStatesAction
+from robot_controllers_msgs.msg import QueryControllerStatesGoal
+from robot_controllers_msgs.msg import ControllerState
 
 class ArmControllerReset:
     CONTROLLER_ACTION_NAME = "/query_controller_states"
@@ -16,22 +18,22 @@ class ArmControllerReset:
 
     def __init__(self):
         rospy.loginfo("Connecting to %s..." % self.CONTROLLER_ACTION_NAME)
-        self.controller_client = actionlib.SimpleActionClient\
-                                 (self.CONTROLLER_ACTION_NAME, QueryControllerStatesAction)
+        self.controller_client = actionlib.SimpleActionClient(
+            self.CONTROLLER_ACTION_NAME, QueryControllerStatesAction)
         self.controller_client.wait_for_server()
         rospy.loginfo("Done")
 
         rospy.loginfo("Connecting to %s..." % self.GRIPPER_ACTION_NAME)
-        self.gripper_client = actionlib.SimpleActionClient\
-                              (self.GRIPPER_ACTION_NAME, GripperCommandAction)
+        self.gripper_client = actionlib.SimpleActionClient(
+            self.GRIPPER_ACTION_NAME, GripperCommandAction)
 
         self.gravity_compsensation = list()
         self.gravity_compsensation.append("arm_controller/gravity_compensation")
 
         self.follow_joint_trajectory = list()
         self.follow_joint_trajectory.append("arm_controller/follow_joint_trajectory")
-        self.follow_joint_trajectory.append\
-            ("arm_with_torso_controller/follow_joint_trajectory")
+        self.follow_joint_trajectory.append(
+            "arm_with_torso_controller/follow_joint_trajectory")
         self.follow_joint_trajectory.append("torso_controller/follow_joint_trajectory")
         self.follow_joint_trajectory.append("head_controller/follow_joint_trajectory")
         self.follow_joint_trajectory.append("head_controller/point_head")
@@ -82,6 +84,7 @@ class ArmControllerReset:
         # goal_servo_off.command.max_effort = -1.0
         # self.gripper_client.send_goal(goal_servo_off)
 
-# if __name__=="__main__":
-#     rospy.init_node("change_controller_state")
-#     c = ControllerStateChange()
+if __name__=="__main__":
+    rospy.init_node("change_controller_state")
+    c = ArmControllerReset()
+    c.servo_on()
