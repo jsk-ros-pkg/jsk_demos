31387d10658d8d8b1e9d3f45fb15dddcbc82abb7
Naoto Tsukamoto <naototukka0413@gmail.com>
2021-12-08 23:26:23 +0900

    add class version

--- jsk_spot_patrol/scripts/mosaic.py
+++ jsk_spot_patrol/scripts/mosaic.py
@@ -1,9 +1,10 @@
-#!/usr/vin/env python
+#!/usr/bin/env python
 
 import rospy
 import cv2
 import numpy as np
 from sensor_msgs.msg import Image
+from jsk_recognition_msgs.msg import RectArray
 from cv_bridge import CvBridge
 
 def mosaic(msg, ratio=0.1):
@@ -57,58 +58,102 @@ def mosaic_area_around(msg):
         #dst[y:y + height, x:x + width] = mosaic(dst[y:y + height, x:x + width], ratio)
         #dst[0:20, 0:20] = mosaic(dst[0:20], ratio)
         #img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
-        small = cv2.resize(
-            dst[0:max_height, 0:x], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+        if rects_list:
+            for l in rects_list:
+                small = cv2.resize(
+                    dst[0:max_height, 0:l.x], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+                )
+                large = cv2.resize(
+                    small, dst[0:max_height, 0:l.x].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+                )
+                dst[0:max_height, 0:l.x] = large
+                
+                small = cv2.resize(
+                    dst[0:max_height, l.x+l.width:max_width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+                )
+                large = cv2.resize(
+                    small, dst[0:max_height, l.x+l.width:max_width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
         )
-        large = cv2.resize(
-            small, dst[0:max_height, 0:x].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
-        )
-        dst[0:max_height, 0:x] = large
+                dst[0:max_height, l.x+l.width:max_width] = large
         
-        small = cv2.resize(
-            dst[0:max_height, x+width:max_width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
-        )
-        large = cv2.resize(
-            small, dst[0:max_height, x+width:max_width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
-        )
-        dst[0:max_height, x+width:max_width] = large
+                small = cv2.resize(
+                    dst[0:l.y, l.x:l.x+width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+                )
+                large = cv2.resize(
+                    small, dst[0:l.y, l.x:l.x+l.width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+                )
+                dst[0:l.y, l.x:l.x+l.width] = large
+                cv2.imshow('image', large)
+                small = cv2.resize(
+                    dst[l.y+l.height:max_height, l.x:l.x+l.width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+                )
+                large = cv2.resize(
+                    small, dst[l.y+l.height:max_height, l.x:l.x+l.width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+                )
+                dst[l.y+l.height:max_height, l.x:l.x+l.width] = large
+        else:
+            print "none of rects"
+        # small = cv2.resize(
+        #     dst[0:max_height, 0:x], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+        # )
+        # large = cv2.resize(
+        #     small, dst[0:max_height, 0:x].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+        # )
+        # dst[0:max_height, 0:x] = large
         
-        small = cv2.resize(
-            dst[0:y, x:x+width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
-        )
-        large = cv2.resize(
-            small, dst[0:y, x:x+width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
-        )
-        dst[0:y, x:x+width] = large
-        cv2.imshow('image', large)
-        small = cv2.resize(
-            dst[y+height:max_height, x:x+width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
-        )
-        large = cv2.resize(
-            small, dst[y+height:max_height, x:x+width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
-        )
-        dst[y+height:max_height, x:x+width] = large
-        cv2.imshow('image', large)
+        # small = cv2.resize(
+        #     dst[0:max_height, x+width:max_width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+        # )
+        # large = cv2.resize(
+        #     small, dst[0:max_height, x+width:max_width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+        # )
+        # dst[0:max_height, x+width:max_width] = large
+        
+        # small = cv2.resize(
+        #     dst[0:y, x:x+width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+        # )
+        # large = cv2.resize(
+        #     small, dst[0:y, x:x+width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+        # )
+        # dst[0:y, x:x+width] = large
+        # cv2.imshow('image', large)
+        # small = cv2.resize(
+        #     dst[y+height:max_height, x:x+width], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST
+        # )
+        # large = cv2.resize(
+        #     small, dst[y+height:max_height, x:x+width].shape[:2][::-1], interpolation=cv2.INTER_NEAREST
+        # )
+        # dst[y+height:max_height, x:x+width] = large
+        # cv2.imshow('image', large)
         cv2.imshow('image_area', dst)
         cv2.waitKey(1)
     except Exception as err:
         print err
 
+def cut_area (msg):
+    global rects_list
+    rects_list = msg.rects
+    # if rects_list:
+    #     print rects_list
+    # else:
+    #     print "none of rects"
+
 def start_node():
-    global x, y, width, height,ratio
+    global x, y, width, height,ratio, rects_list
     rospy.init_node('img_proc')
     rospy.loginfo('img_proc node started')
     #rospy.Subscriber("/kinova_wrist_camera/color/image_raw", Image, mosaic)
     #rospy.Subscriber("/kinova_wrist_camera/color/image_raw", Image, mosaic_area_around)
-    rospy.Subscriber("/dual_fisheye_to_panorama/output", Image, mosaic_area_around)
+    rospy.Subscriber("/spot_recognition/rects", RectArray, cut_area)
+    rospy.Subscriber("/spot_recognition/object_detection_image", Image, mosaic_area_around)
     # x = 50
     # y = 50
     # width = 400
     # height = 400
-    x = rospy.get_param("~target_x", 2000)
+    x = rospy.get_param("~target_x", 50)
     y = rospy.get_param("~target_y", 50)
-    width = rospy.get_param("~width", 1000)
-    height = rospy.get_param("~height", 1000)
+    width = rospy.get_param("~width", 500)
+    height = rospy.get_param("~height", 300)
     ratio = 0.1
     print "hoge"
     rospy.spin()
