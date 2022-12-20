cf517161682125073a3d6781c5b6453a89e665bd
Ayumu Iwata <iwata-ayumu622@gcc.u-tokyo.ac.jp>
2022-12-20 20:18:05 +0900

    fix for walking

--- jsk_2022_10_semi/ayumu_demo/node_distance.py
+++ jsk_2022_10_semi/ayumu_demo/node_distance.py
@@ -9,6 +9,7 @@ pub = rospy.Publisher('nearest_pos', Float32MultiArray, queue_size=1)
 def callback(msg):
     boxes = msg.boxes
 
+    # there is no person around the camera
     if (not boxes):
         rospy.loginfo("no person")
         return -1
@@ -16,7 +17,8 @@ def callback(msg):
     length = len(boxes)
     min_distance = 10000
     min_ind = 0
-  
+
+    # identify the person closest to the camera 
     for i in range(length):
         position = boxes[i].pose.position
         distance = position.x*position.x +position.y*position.y
@@ -30,17 +32,15 @@ def callback(msg):
 
     if ((-0.6 <= coordinate[0]) and (coordinate[0] < 1.5)):
        if ((0.3 < coordinate[1]) and (coordinate[1] < 0.6)):
-           position_number = 1 # left back
+           position_number = 1 # left
        elif ((-0.6 < coordinate[1]) and (coordinate[1] < -0.3)):
-           position_number = 2 # right back
-            
-
+           position_number = 2 # right
 
     min_position = []
     min_position.append(position_number)
-    min_position.append(boxes[min_ind].pose.position.x)
-    min_position.append(boxes[min_ind].pose.position.y)
-    min_position.append(boxes[min_ind].pose.position.z)
+    min_position.append(coordinate[0])
+    min_position.append(coordinate[1])
+    min_position.append(coordinate[2])
     min_position.append(min_distance)
 
     min_forPublish = Float32MultiArray(data=min_position)
