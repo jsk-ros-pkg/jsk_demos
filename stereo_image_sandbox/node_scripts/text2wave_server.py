817ef03eec3733d663ce88883cc94fcc31b14c78
Iori Yanokura <ab.ioryz@gmail.com>
2024-03-17 12:31:35 +0900

    Add

--- stereo_image_sandbox/node_scripts/text2wave_server.py
+++ stereo_image_sandbox/node_scripts/text2wave_server.py
@@ -110,4 +110,4 @@ async def text_to_speech(request: SpeechRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8075)
