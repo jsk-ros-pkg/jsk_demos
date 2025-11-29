71d0fa61a13e90a785a324c688d446921ac30a2f
sawada10 <sawada@jsk.imi.i.u-tokyo.ac.jp>
2025-11-29 12:36:12 +0900

    add kashiwagi voice when moving

--- jsk_2025_05_kashiwagi/src/node_scripts/kashiwagi_speaker.py
+++ jsk_2025_05_kashiwagi/src/node_scripts/kashiwagi_speaker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 import actionlib
-from std_msgs.msg import String, Float32
+from std_msgs.msg import String
 from sound_play.msg import SoundRequestAction, SoundRequestGoal, SoundRequest
 import time
 
@@ -10,22 +10,24 @@ class KashiwagiSpeaker:
         rospy.init_node('kashiwagi_speaker')
         self.cur_state = "unknown"
         self.prev_state = "unknown"
+        self.last_play_time = 0.0  # 最後に再生した時間
+        self.repeat_interval = 3.0 # move:happy時の繰り返し秒数
         rospy.Subscriber('/kashiwagi_state', String, self.state_callback)
         rospy.loginfo("Launching kashiwagi speaker node ....")
+
         self.client = actionlib.SimpleActionClient('/robotsound_jp', SoundRequestAction)
         self.client.wait_for_server()
-        self.state_updated = False
+
+        # 定期チェック用タイマー
+        rospy.Timer(rospy.Duration(0.5), self.timer_callback)
+
         rospy.spin()
 
     def state_callback(self, msg):
-        print("cur, prev", self.cur_state, self.prev_state)
         self.prev_state = self.cur_state
         self.cur_state = msg.data
 
-        if self.prev_state == self.cur_state:
-            self.state_updated = False
-        else:
-            self.state_updated = True
+        self.state_updated = (self.prev_state != self.cur_state)
 
         wav_file = None
 
@@ -36,15 +38,31 @@ class KashiwagiSpeaker:
                 wav_file = "/home/ubuntu/ros/kashiwagi_ws/src/jsk_demos/jsk_2025_05_kashiwagi/data/kashiwagi_hmm.wav"
             elif self.cur_state == "move:getting_lost":
                 wav_file = "/home/ubuntu/ros/kashiwagi_ws/src/jsk_demos/jsk_2025_05_kashiwagi/data/kashiwagi_megamawaru.wav"
+            elif self.cur_state == "move:goal":
+                wav_file = "/home/ubuntu/ros/kashiwagi_ws/src/jsk_demos/jsk_2025_05_kashiwagi/data/kashiwagi_kitayo.wav"
+            elif self.cur_state == "move:approaching_person":
+                wav_file = "/home/ubuntu/ros/kashiwagi_ws/src/jsk_demos/jsk_2025_05_kashiwagi/data/kashiwagi_yoisho.wav"
+                self.last_play_time = time.time()
+
             if wav_file:
                 self.play_wav(wav_file)
 
+    def timer_callback(self, event):
+        if self.cur_state != "move:approaching_person":
+            return
+
+        now = time.time()
+        if now - self.last_play_time >= self.repeat_interval:
+            wav_file = "/home/ubuntu/ros/kashiwagi_ws/src/jsk_demos/jsk_2025_05_kashiwagi/data/kashiwagi_yoisho.wav"
+            self.play_wav(wav_file)
+            self.last_play_time = now
+
     def play_wav(self, file_path):
         goal = SoundRequestGoal()
-        goal.sound_request.sound = SoundRequest.PLAY_FILE  # ファイル再生モード
-        goal.sound_request.command = SoundRequest.PLAY_ONCE  # 一回だけ再生
-        goal.sound_request.arg = file_path  # 再生する wav ファイルのパス
-        goal.sound_request.volume = 1.0     # 音量（0.0〜1.0）
+        goal.sound_request.sound = SoundRequest.PLAY_FILE
+        goal.sound_request.command = SoundRequest.PLAY_ONCE
+        goal.sound_request.arg = file_path
+        goal.sound_request.volume = 1.0
 
         rospy.loginfo(f"Playing: {file_path}")
         self.client.send_goal(goal)
